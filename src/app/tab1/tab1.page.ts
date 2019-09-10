0ef8b090dcefed4aa936738ef241f0de200684f1
juankmiloh <juankmiloh@hotmail.com>
2019-09-09 19:47:49 -0500

    cargar cambios

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,47 @@ export class Tab1Page {
 
   constructor() {}
 
+  usuario = [{ nombre: 'Juan Camilo Herrera Ardila',
+               pass: 1234,
+               estado: true
+             },
+             { nombre: 'Ana Maria Herrera Moreno',
+               pass: 12345,
+               estado: false
+             }
+            ];
+
+  lugar = [
+            { name: 'Barranquilla',
+              // tslint:disable-next-line: max-line-length
+              description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+              image: 'assets/image/barranquilla.jpg',
+              status: true
+            },
+            { name: 'Bogotá D.C.',
+              // tslint:disable-next-line: max-line-length
+              description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+              image: 'assets/image/bogota.png',
+              status: true
+            },
+            { name: 'Bucaramanga',
+              // tslint:disable-next-line: max-line-length
+              description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+              image: 'assets/image/bucaramanga.png',
+              status: true
+            },
+            { name: 'Cali',
+              // tslint:disable-next-line: max-line-length
+              description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+              image: 'assets/image/cali.jpg',
+              status: true
+            },
+            { name: 'Cartagena',
+              // tslint:disable-next-line: max-line-length
+              description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+              image: 'assets/image/cartagena.jpg',
+              status: true
+            }
+          ];
+
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,4 +9,74 @@ export class Tab3Page {
 
   constructor() {}
 
+  mostrarCategoria = true;
+  categoria = null;
+
+  onClickFunction(page: number){
+    this.mostrarCategoria = false;
+    this.categoria = page;
+  }
+
+  back(){
+    this.mostrarCategoria = true;
+  }
+
+  data = 
+    [
+      {
+        "id": 0,
+        "name": "Gastronomia",
+        "imagen": 'assets/image/gastronomia.png', 
+        "card": [ 
+                  {
+                    nombre : 'card1',
+                    // tslint:disable-next-line: max-line-length
+                    descripcion : 'Según la historia el pueblo se llamaba Porto Rico (puerto rico), hasta que el 1850 se convirtió en un paraje donde se desembarcaban y comer',
+                    imagen: 'assets/image/cartagena.jpg'
+                  },
+                  {
+                    nombre : 'card2',
+                    // tslint:disable-next-line: max-line-length
+                    descripcion : 'Según la historia el pueblo se llamaba Porto Rico (puerto rico), hasta que el 1850 se convirtió en un paraje donde se desembarcaban y comer',
+                    imagen: 'assets/image/cartagena.jpg'
+                  },         
+                ]
+      },
+      {
+        "id": 1,
+        "name": "Playas",
+        "imagen": 'assets/image/playas.png',
+        "card": [ {
+            nombre : 'card2',
+            // tslint:disable-next-line: max-line-length
+            descripcion : 'Según la historia el pueblo se llamaba Porto Rico (puerto rico), hasta que el 1850 se convirtió en un paraje donde se desembarcaban y comer',
+            imagen: 'assets/image/cartagena.jpg'
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Gastronomia 2",
+        "imagen": 'assets/image/gastronomia2.png',
+        "card": [ {
+            nombre : 'card3',
+            // tslint:disable-next-line: max-line-length
+            descripcion : 'Según la historia el pueblo se llamaba Porto Rico (puerto rico), hasta que el 1850 se convirtió en un paraje donde se desembarcaban y comer',
+            imagen: 'assets/image/cartagena.jpg'
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "name": "Tendencias",
+        "imagen": 'assets/image/tendencias.png',
+        "card": [ {
+            nombre : 'card4',
+            // tslint:disable-next-line: max-line-length
+            descripcion : 'Según la historia el pueblo se llamaba Porto Rico (puerto rico), hasta que el 1850 se convirtió en un paraje donde se desembarcaban y comer',
+            imagen: 'assets/image/cartagena.jpg'
+          }
+        ]
+      },
+    ]
 }
